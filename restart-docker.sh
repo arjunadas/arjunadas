fb95fba7d30bc32467a29d27a4068d3b80ab061a
unknown <C:\Users\alex\AppData\Roaming\The Bat!>
2023-09-22 06:36:14 +0300

    old

--- restart-docker.sh
+++
@@ -1,6 +0,0 @@
-# get all names of the running containers
-b=(`docker ps | awk '{print $ 13}' | tail -n +2`)
-# copy certificates into containers
-for name in ${b[@]}; do docker cp /opt/certupdate/tempcert/cert.pfx $name:/app ; done
-# restart all containers
-for name in ${b[@]}; do docker restart $name ; done
